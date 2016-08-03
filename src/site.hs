2d4131694b79ff4e56d5e664cb348d1de3603cea
Kevin Li <kevin.li195@hotmail.com>
2016-08-03 11:44:07 -0400

    implement new route that removes src dir

--- src/site.hs
+++ src/site.hs
@@ -2,34 +2,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Hakyll.Core.Routes
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
     match "src/images/*" $ do
-        route   idRoute
+        route   rmSrcRoute
         compile copyFileCompiler
 
     match "src/css/*" $ do
-        route   idRoute
+        route   rmSrcRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
+    match (fromList ["src/about.rst", "src/contact.markdown"]) $ do
+        route   $ setExtension "html" <.> rmSrcRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "src/templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "src/posts/*" $ do
-        route $ setExtension "html"
+        route $ setExtension "html" <.> rmSrcRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "src/templates/post.html"    postCtx
             >>= loadAndApplyTemplate "src/templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["src/archive.html"] $ do
-        route idRoute
+    create ["archive.html"] $ do
+        route rmSrcRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
@@ -37,14 +40,14 @@ main = hakyll $ do
                     constField "title" "Archives"            `mappend`
                     defaultContext
 
-            makeItem ""
+            makeItem "" -- this is the empty item w/ type String
                 >>= loadAndApplyTemplate "src/templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "src/templates/default.html" archiveCtx
                 >>= relativizeUrls
 
 
     match "src/index.html" $ do
-        route idRoute
+        route rmSrcRoute
         compile $ do
             posts <- recentFirst =<< loadAll "src/posts/*"
             let indexCtx =
@@ -65,3 +68,20 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+rmSrcRoute :: Routes
+rmSrcRoute = customRoute removeSrc
+
+srcParser :: Parser FilePath
+srcParser = do
+    string "src"
+    anyChar `manyTill` eof
+
+removeSrc :: Identifier -> FilePath
+removeSrc i = case runParser srcParser $ toFilePath i of
+    Left _ -> toFilePath i
+    Right s -> s
+
+-- | Convenience function for 'Routes' composition.
+(<.>) :: Routes -> Routes -> Routes
+a <.> b = a `composeRoutes` b
