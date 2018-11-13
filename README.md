d1e81a4d0f0a2efea90bf25543ed6d8ec356bf6c
Kevin Li <github@mail.kevinl.io>
2018-11-12 19:53:20 -0500

    stackify and add yield curve

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # blog
-Blog generated with Hakyll
-
-TODO
-=====
-- Remove dates in filenames
-- Reconfigure nginx to take no ".html"
--- src/site.hs
+++ src/site.hs
@@ -1,76 +1,83 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
-import           Hakyll.Core.Routes
-import           Hakyll.Core.Identifier
-import           Text.Pandoc
-import           Text.Megaparsec
-import           Text.Megaparsec.Char
-import           Text.Megaparsec.String
+import Data.Monoid (mappend)
+import Data.Void
 
+import GHC.IO.Encoding
+
+import Hakyll
+import Hakyll.Core.Routes
+import Hakyll.Core.Identifier
+import Text.Pandoc
+import Text.Megaparsec hiding ( match )
+import Text.Megaparsec.Char
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
-    match "src/images/*" $ do
-        route   rmSrcRoute
-        compile copyFileCompiler
+main = do
+    setLocaleEncoding utf8 
+    hakyll $ do
+      match "src/images/*" $ do
+          route   rmSrcRoute
+          compile copyFileCompiler
+      match "src/css/*" $ do
+          route   rmSrcRoute
+          compile compressCssCompiler
+      match "src/charts/*" $ do
+          route rmSrcRoute
+          compile copyFileCompiler
+      match "src/contact.markdown" $ do
+          route   $ setExtension "html" <.> rmSrcRoute
+          compile $ pandocMathCompiler
+              >>= loadAndApplyTemplate "src/templates/default.html" defaultContext
+              >>= relativizeUrls
+      match "src/posts/*" $ do
+          route $ setExtension "html" <.> rmSrcRoute
+          compile $ pandocCompiler
+              >>= loadAndApplyTemplate "src/templates/post.html"    postCtx
+              >>= loadAndApplyTemplate "src/templates/default.html" postCtx
+              >>= relativizeUrls
+      create ["archive.html"] $ do
+          route rmSrcRoute
+          compile $ do
+              posts <- recentFirst =<< loadAll "src/posts/*"
+              let archiveCtx =
+                      listField "posts" postCtx (return posts) `mappend`
+                      constField "title" "Archives"            `mappend`
+                      defaultContext
 
-    match "src/css/*" $ do
-        route   rmSrcRoute
-        compile compressCssCompiler
+              makeItem "" -- this is the empty item w/ type String
+                  >>= loadAndApplyTemplate "src/templates/archive.html" archiveCtx
+                  >>= loadAndApplyTemplate "src/templates/default.html" archiveCtx
+                  >>= relativizeUrls
+      create ["macro-list.html"] $ do
+          route rmSrcRoute
+          compile $ do
+              charts <- recentFirst =<< loadAll ("src/charts/*" .&&. complement "src/charts/*")
+              let chartsCtx =
+                      listField "charts" defaultContext (return charts) `mappend`
+                      constField "title" "Macro Charts" `mappend`
+                      defaultContext
 
-    match "src/contact.markdown" $ do
-        route   $ setExtension "html" <.> rmSrcRoute
-        compile $ pandocMathCompiler
-            >>= loadAndApplyTemplate "src/templates/default.html" defaultContext
-            >>= relativizeUrls
+              makeItem "" -- this is the empty item w/ type String
+                  >>= loadAndApplyTemplate "src/templates/macro-list.html" chartsCtx
+                  >>= loadAndApplyTemplate "src/templates/default.html" chartsCtx
+                  >>= relativizeUrls
+      match "src/index.html" $ do
+          route rmSrcRoute
+          compile $ do
+              posts <- recentFirst =<< loadAll "src/posts/*"
+              let indexCtx =
+                      listField "posts" postCtx (return posts) `mappend`
+                      constField "title" "Home"                `mappend`
+                      defaultContext
 
-    match "src/posts/*" $ do
-        route $ setExtension "html" <.> rmSrcRoute
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "src/templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "src/templates/default.html" postCtx
-            >>= relativizeUrls
+              getResourceBody
+                  >>= applyAsTemplate indexCtx
+                  >>= loadAndApplyTemplate "src/templates/default.html" indexCtx
+                  >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route rmSrcRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "src/posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem "" -- this is the empty item w/ type String
-                >>= loadAndApplyTemplate "src/templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "src/templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-    -- match "src/cv.md" $ do
-    --     route $ setExtension "html" <.> rmSrcRoute
-    --     compile $ do
-    --         let cvCtx = defaultContext
-    --         pandocCompiler
-    --             >>= loadAndApplyTemplate "src/templates/default.html" cvCtx
-    --             >>= relativizeUrls
-
-    match "src/index.html" $ do
-        route rmSrcRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "src/posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "src/templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "src/templates/*" $ compile templateBodyCompiler
+      match "src/templates/*" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
@@ -82,7 +89,7 @@ postCtx =
 rmSrcRoute :: Routes
 rmSrcRoute = customRoute removeSrc
 
-srcParser :: Parser FilePath
+srcParser :: Parsec Void String FilePath
 srcParser = do
     string "src/"
     anyChar `manyTill` eof
